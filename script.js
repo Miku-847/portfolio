2ef525931c9b124e818f0086940d6a39868ce842
Neha <neha@Nehas-MacBook-Air.local>
2025-12-12 20:44:46 +0000

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector('.custom-cursor');
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeBtn = document.querySelector('.close-button');
+const closeBtn = document.querySelector('.js-close-button');
 
 // for desktop
 document.addEventListener('mousemove', (e) => {
@@ -9,16 +9,7 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.top = e.clientY + 'px';
 });
 
-// for phone (touchmove)
-document.addEventListener('touchmove', (e) => {
-    if (e.touches.length > 0) {
-        cursor.style.left = e.touches[0].clientX + 'px';
-        cursor.style.top = e.touches[0].clientY + 'px';
-    }
-});
-
-
-// header
+// header for mobile layout
 burger.addEventListener('click', () => {
     mobileMenu.classList.toggle('open');
 });
